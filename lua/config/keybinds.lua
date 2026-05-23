dc73bdd0ae946a883b020f2ecd0fe4e0b32f0dd4
Jacob Reger <regerjacob@gmail.com>
2026-05-22 19:56:42 -0700

    very basic autopairs

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,22 +1,38 @@
--- global keybinds
+--------------------------------------------------------------------------------
+--- global keybinds
+--------------------------------------------------------------------------------
+
 -- focus switching
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 
--- nvim-tree
+-- auto-parentheses
+vim.keymap.set("i", "(", "()<left>")
+vim.keymap.set("i", "{", "{}<left>")
+vim.keymap.set("i", "[", "[]<left>")
+vim.keymap.set("i", "\"", "\"\"<left>")
+vim.keymap.set("i", "'", "''<left>")
+
+--------------------------------------------------------------------------------
+--- nvim-tree
+--------------------------------------------------------------------------------
 local tree_api = require("nvim-tree.api")
 vim.keymap.set("n", "<C-n>", tree_api.tree.toggle)
 
--- telescope
+--------------------------------------------------------------------------------
+--- telescope
+--------------------------------------------------------------------------------
 local tele_api = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", tele_api.find_files)
 vim.keymap.set("n", "<leader>fw", tele_api.live_grep)
 vim.keymap.set("n", "<leader>fb", tele_api.buffers)
 vim.keymap.set("n", "<leader>th", tele_api.colorscheme)
 
--- lsp keybinds
+--------------------------------------------------------------------------------
+--- lsp
+--------------------------------------------------------------------------------
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
